fcf4e700c81e63c1692d773a99f410bff61efc58
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-23 16:31:20 +0000

    Set comment filter

--- profiles/views.py
+++ profiles/views.py
@@ -20,6 +20,8 @@ class ProfileList(generics.ListCreateAPIView):
 
     filterset_fields = [
         'owner__following__followed__profile'
+        'owner__followed__owner__profile'
+        'owner__followed__owner__profile'
     ]
 
     ordering_fields = [
